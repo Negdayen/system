945401fabadad061cdf7d90b85342dd343766d24
Jacob Horrocks <Jacob.Horrocks@gmail.com>
2021-07-18 09:37:38 -0700

    Fix home CPATH expansion.

--- roles/base/files/shells/home/.profile.d/home-paths.sh
+++ roles/base/files/shells/home/.profile.d/home-paths.sh
@@ -1,4 +1,4 @@
-export CPATH=":{HOME}/.local/include:${CPATH}"
+export CPATH=":${HOME}/.local/include:${CPATH}"
 export INFOPATH="${HOME}/.local/share/info:${INFOPATH}"
 export LD_LIBRARY_PATH="${HOME}/.local/lib:${LD_LIBRARY_PATH}"
 export LIBRARY_PATH="${HOME}/.local/lib:${LIBRARY_PATH}"
